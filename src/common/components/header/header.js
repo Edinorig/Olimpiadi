df531855aac2a20af285b706a72f32893fa3e3e2
EDOARDO QUERCIOLI <quercioli.edoardo@volta.ts.it>
2023-02-14 09:23:10 +0100

    cleanup ;)

--- src/common/components/header/header.js
+++ src/common/components/header/header.js
@@ -3,7 +3,28 @@ class Header extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         if (this.shadowRoot != null)
-            this.shadowRoot.innerHTML = ``;
+            this.shadowRoot.innerHTML = `
+            <style>
+            body{
+                margin: 0;
+            }
+            .header{
+                position: fixed;
+                top: 0;
+                left: 0;
+                height: 120px;
+                width: 100%;
+                display: flex;
+                justify-content: start;
+                align-items: center;
+            }
+            .logo{
+                height: 100%;
+            }
+            </style>
+            <div class = "header">
+                <a href="../../index.php" class = "logo"><img src = "../common/assets/logo.png" class = "logo"></a>
+            </div>`;
     }
 }
 customElements.define('header-component', Header);
